6f334052745f0c02bf21ba6ad35d060d9acacc75
MRcraigchick <craigchick@outlook.com>
2022-11-22 11:54:24 +0100

    update

--- api/backend_request.js
+++ api/backend_request.js
@@ -1,7 +1,7 @@
 // Change this to your own IP address if running the server on your
 // local machine else change to the URL of the deployed server
 
-export const serverURL = `http://192.168.0.14:3000`;
+export const serverURL = `http://192.168.10.199:3000`;
 
 function constructURL(endpoint) {
   return `${serverURL}${endpoint}`;
